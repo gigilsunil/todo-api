dc06aa5e24b1d335ba42a7746db97541a60011f9
Gigil Mathew <gigil.sunil@gmail.com>
2017-02-07 13:47:57 +1000

    added todo based on id

--- server.js
+++ server.js
@@ -1,6 +1,46 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT ||3000;
+var todos = [
+{
+	id : 1,
+	description : 'to learn',
+	completed : false
+},
+{
+	id : 2,
+	description : 'shopping',
+	completed : true
+},
+{
+	id : 3,
+	description : 'post the letter',
+	completed : false
+}];
+
+app.get('/todos',function(req,res)
+{
+	res.json(todos);
+});
+//request.params.id
+app.get('/todos/:id',function(req,res)
+{
+	var id = parseInt(req.params.id);
+	todos.forEach(function(x)
+	{
+		matchedId =0;
+		if(typeof x == 'object' && x.id === id)
+		{
+			matchedId=1;
+			res.json(x);
+
+		}
+	});
+	if(matchedId === 0)
+	{
+		res.status(404).send();
+	}
+});
 
 app.get('/',function(req,res)
 {
